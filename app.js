6841af98564e5b0d9fdd04fddb735ddd6bb2777b
piisuke <piisuke@kandr.biz>
2022-02-06 15:38:26 +0900

    ファイルからデータを抜き出す

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 'use strict';
 const fs = require('fs');
+const readline = require('readline');
+const rs = fs.createReadStream('./popu-pref.csv');
+const rl=  readline.createInterface({input: rs, output:{}})
+rl.on('line',lineString =>{
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = parseInt(columns[1]);
+    const prefecture = columns[1];
+    const popu = parseInt(columns[3]);
+    
+    
+
+    if(year == 2010 || year == 2015){
+        console.log(year);
+        console.log(prefecture);
+        console.log(popu);
+        
+    }
+});
+
+
 
